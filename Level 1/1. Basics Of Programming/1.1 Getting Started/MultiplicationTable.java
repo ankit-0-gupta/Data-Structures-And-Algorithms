9d618c09303f9a3eb192cfef846f6d5eebcf8297
Ankit Gupta <112970191+ankit-gupta-00@users.noreply.github.com>
2023-07-16 22:46:29 +0530

    Multiplication Table

---
+++ Level 1/1. Basics Of Programming/1.1 Getting Started/MultiplicationTable.java
@@ -0,0 +1,47 @@
+// Multiplication Table of a number.
+
+// Input Format : 
+
+// A number n
+
+// Output Format :
+
+// Multiplication Table of n
+
+// Sample Input : 
+
+// 5
+
+// Sample Output :
+
+// 5 * 1 = 5
+// 5 * 2 = 10
+// 5 * 3 = 15
+// 5 * 4 = 20
+// 5 * 5 = 25
+// 5 * 6 = 30
+// 5 * 7 = 35
+// 5 * 8 = 40
+// 5 * 9 = 45
+// 5 * 10 = 50
+
+
+import java.util.Scanner;
+public class MultiplicationTable
+{
+    // Multiplication Table.
+    public static void multiplicationTable(int n)
+    {
+        for(int i = 1; i <= 10; i++)
+        {
+            System.out.println(n + " * " + i + " = " + (n * i));
+        }
+    }
+
+    public static void main(String[] args)
+    {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        multiplicationTable(n);
+    }
+}
