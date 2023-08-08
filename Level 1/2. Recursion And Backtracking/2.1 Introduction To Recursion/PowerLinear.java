67a366a8b307ba05285b5136cc91f5844b6e1507
Ankit Gupta <112970191+ankit-gupta-00@users.noreply.github.com>
2023-08-08 10:56:56 +0530

    Power - Linear

---
+++ Level 1/2. Recursion And Backtracking/2.1 Introduction To Recursion/PowerLinear.java
@@ -0,0 +1,50 @@
+// Power - Linear.
+
+// 1. You are given a number x.
+// 2. You are given another number n.
+// 3. You are required to calculate x raised to the power n.
+
+// Note : The online judge can't force you to write the function recursively but that is what the spirit of question is. 
+//        Write recursive and not iterative logic. The purpose of the question is to aid learning recursion and not test you.
+
+// Input Format :
+
+// A number x
+// A number n
+
+// Output Format :
+
+// x raised to the power n
+
+// Constraints :
+
+// 1 <= x <= 10
+// 0 <= n <= 9
+
+// Sample Input :
+
+// 2
+// 5
+
+// Sample Output :
+
+// 32
+
+
+import java.util.Scanner;
+public class PowerLinear
+{
+    // Power - Linear.
+    public static int powerLinear(int x, int n)
+    {
+        return (n == 0) ? 1 : x * powerLinear(x, n - 1);
+    }
+
+    public static void main(String[] args)
+    {
+        Scanner scn = new Scanner(System.in);
+        int x = scn.nextInt();
+        int n = scn.nextInt();
+        System.out.print(powerLinear(x, n));
+    }
+}
